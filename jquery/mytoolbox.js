946e6588d8060fb9f1182c08076f6b846dcfc97e
Louiezheng <louie.zheng@zurich.com>
2014-07-14 18:23:10 +0800

    first

---
+++ jquery/mytoolbox.js
@@ -0,0 +1,13 @@
+jQuery.fn.mytoolbox = function() {
+    return this.each(function() {
+      jQuery(this).click(function () {
+            alert(this.id);
+
+             //$('#test1').fadeOut();
+             
+			$(this).fadeOut();
+
+
+        });
+    });
+};
